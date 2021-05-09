586f0320890622a1aa06746d63de7a66f3fca984
guy <52482940+guyford94@users.noreply.github.com>
2021-05-09 16:02:15 +0300

    Create Clock.c

---
+++ Clock.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <Windows.h>
+
+
+int main() {
+
+	int h, m, s, d = 1000;
+	printf("Set time: \n");
+	scanf_s("%d%d%d", &h, &m, &s);
+	if (h > 12 || m > 60 || s > 60) {
+		printf("ERROR ! \n");
+		exit(0);
+	}
+
+	while (1) {
+		s++;
+		if (s > 59) {
+			m++;
+			s = 0;
+		}
+		if (m > 59) {
+			h++;
+			m = 0;
+		}
+		if (h > 12) {
+			h = 1;
+		}
+		printf("\n Clock:");
+		printf("\n %02d:%02d:%02d", h, m, s);
+		Sleep(d);
+		system("cls");
+		
+	
+	}
+
+}
